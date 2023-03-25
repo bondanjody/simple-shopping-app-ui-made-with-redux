14cd70ee1241333322382ef227284ba5fe090b16
bondanjody <bondanjodys@gmail.com>
2023-03-25 14:24:02 +0700

    Melengkapi bagian cart-slice (bagian removeItem). Membuat DUMMY_DATA. Membuat badge bekerja sesuai action kita (berubah secara dinamis ketika kita memencet Add to Cart)

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,16 +1,36 @@
 import ProductItem from './ProductItem';
 import classes from './Products.module.css';
 
+const DUMMY_DATA = [
+  {
+    id: 'p1',
+    price: 21,
+    title: 'Harry Potter and The Deathly Hollows',
+    description: 'The Final Book of Harry Potter series'
+  },
+  {
+    id: 'p2',
+    price: 19,
+    title: 'Harry Potter and The Philosopher Stone',
+    description: 'The First Book of Harry Potter series'
+  }
+];
+
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {DUMMY_DATA.map((data) => 
+            <ProductItem
+              key={data.id}
+              id={data.id}
+              title={data.title}
+              price={data.price}
+              description={data.description}
+          />
+        )}
+        
       </ul>
     </section>
   );
--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -10,6 +10,7 @@ const cartSlice = createSlice({
         addItemToCart(state, action) {
             const newItem = action.payload;
             const existingItem = state.items.find(item => item.id === newItem.id);
+            state.totalQuantity++;
             if (!existingItem) {
                 // Jika produk belum ada di cart
                 state.items.push({
@@ -25,11 +26,20 @@ const cartSlice = createSlice({
                 existingItem.totalPrice = existingItem.totalPrice + newItem.totalPrice;
             }
         },
-        removeItemFromCart() {
-
+        removeItemFromCart(state, action) {
+            const id = action.payload;
+            const existingItem = state.items.find(item => item.id === id);
+            existingItem.totalQuantity--;
+            if (existingItem.quantity === 1) {
+                state.items = state.items.filter(item => item.id !== id);
+            } else {
+                existingItem.quantity--;
+                existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
+            }
         }
     }
 });
 
+export const cartActions = cartSlice.actions;
 export default cartSlice;
 
