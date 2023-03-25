f61907c6fe5a220f2c17355cfb6044d176f71183
bondanjody <bondanjodys@gmail.com>
2023-03-25 16:30:19 +0700

    Menambahkan komponen baru Notification yang fungsinya menampilkan Notifikasi pengiriman data (fetch). Menangani HTTP states dan Feedback dengan Redux

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { useEffect } from 'react';
+import { uiActions } from './store/ui-slice';
+import { Fragment } from 'react';
+import Notification from './components/UI/Notification';
+
+let isInitial = true;
 
 function App() {
   const showCart = useSelector(state => state.ui.cartIsVisible);
   const cart = useSelector(state => state.cart);
+  const notification = useSelector(state => state.ui.notification);
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    fetch('https://react-http-request-ab44d-default-rtdb.firebaseio.com/cart.json', {
-      method: 'PUT',
-      body: JSON.stringify(cart)
-    });
-  }, [cart]);
+    const sendCartData = async () => {
+      dispatch(uiActions.showNotification({
+        status: 'pending',
+        title:'Sending...',
+        message:'Sending cart data.'
+      }));
+
+      const response = await fetch('https://react-http-request-ab44d-default-rtdb.firebaseio.com/cart.json', {
+        method: 'PUT',
+        body: JSON.stringify(cart)
+      });
+      
+      if (!response.ok) {
+        throw new Error('Sending cart data FAILED.');
+      }
+
+      dispatch(uiActions.showNotification({
+        status: 'success',
+        title:'Success!',
+        message:'Sent cart data successfully.'
+      }));
+    }
+
+    if (isInitial) {
+      isInitial = false;
+      return;
+    }
+
+    sendCartData().catch(error => {
+      dispatch(uiActions.showNotification({
+        status: 'error',
+        title:'Error!',
+        message:'Sending cart data FAILED.'
+      }));
+    })
+    
+  }, [cart, dispatch]);
 
   return (
-    <Layout>
-      {showCart && <Cart />}
-      <Products />
-    </Layout>
+    <Fragment>
+      {notification && <Notification status={notification.status} message={notification.message} title={notification.title} />}
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </Fragment>
+    
   );
 }
 
