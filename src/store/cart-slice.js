be9b0d6f9ee0dd5edccd2e179fa4741e71c33776
bondanjody <bondanjodys@gmail.com>
2023-03-25 14:43:06 +0700

    Menyempurnakan fungsionalitas button pada Cart Item (Tombol + dan -)

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -14,7 +14,7 @@ const cartSlice = createSlice({
             if (!existingItem) {
                 // Jika produk belum ada di cart
                 state.items.push({
-                    itemId: newItem.id,
+                    id: newItem.id,
                     price: newItem.price,
                     quantity: 1,
                     totalPrice: newItem.price,
@@ -23,13 +23,13 @@ const cartSlice = createSlice({
             } else {
                 // Jika produk sudah ada di cart
                 existingItem.quantity++;
-                existingItem.totalPrice = existingItem.totalPrice + newItem.totalPrice;
+                existingItem.totalPrice = existingItem.totalPrice + newItem.price;
             }
         },
         removeItemFromCart(state, action) {
             const id = action.payload;
             const existingItem = state.items.find(item => item.id === id);
-            existingItem.totalQuantity--;
+            state.totalQuantity--;
             if (existingItem.quantity === 1) {
                 state.items = state.items.filter(item => item.id !== id);
             } else {
